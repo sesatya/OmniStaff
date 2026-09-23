0b956310054614d9f1ba8524bd5ac3c4dba1bc6d
SatyaShrivastava <satya.shrivastava@ibm.com>
2026-09-23 16:20:43 +0530

    removing cascade and regenerating the migration

---
+++ OmniStaff.Infrastructure/Migrations/20260923104259_UpdateFkSetNull_Cascade.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OmniStaff.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateFkSetNull_Cascade : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employees_Employees_ManagerId",
+                table: "Employees");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_LeaveRequests_Employees_ManagerId",
+                table: "LeaveRequests");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employees_Employees_ManagerId",
+                table: "Employees",
+                column: "ManagerId",
+                principalTable: "Employees",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_LeaveRequests_Employees_ManagerId",
+                table: "LeaveRequests",
+                column: "ManagerId",
+                principalTable: "Employees",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employees_Employees_ManagerId",
+                table: "Employees");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_LeaveRequests_Employees_ManagerId",
+                table: "LeaveRequests");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employees_Employees_ManagerId",
+                table: "Employees",
+                column: "ManagerId",
+                principalTable: "Employees",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_LeaveRequests_Employees_ManagerId",
+                table: "LeaveRequests",
+                column: "ManagerId",
+                principalTable: "Employees",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+    }
+}
